d8e810940ac345bd4b31c9303211aa1193a6d994
Dinil Valsan <dinilvalsan@gmail.com>
2023-04-13 15:20:33 +0530

    update wordlist generation

--- word_list_generation_tools/all_possible_combos.bat
+++ word_list_generation_tools/all_possible_combos.bat
@@ -1,16 +1,13 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set /p file1=Enter the name of the first file: 
-set /p file2=Enter the name of the second file: 
-set /p outfile=Enter the name of the output file: 
-
-for %%a in (%file1%) do set file1name=%%~na
-for %%a in (%file2%) do set file2name=%%~na
+set /p file1=Enter the path of the first file: 
+set /p file2=Enter the path of the second file: 
+set /p output=Enter the output filename: 
 
 for /f "delims=" %%a in (%file1%) do (
     for /f "delims=" %%b in (%file2%) do (
-        echo %%a %file1name%, %%b %file2name% >> %outfile%
+        echo %%a,%%b >> %output%
     )
 )
 
